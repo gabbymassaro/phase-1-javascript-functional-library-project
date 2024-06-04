1ea72fd7acc29c4f8c2d3c21d39d5d3a602449c7
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-04 13:10:50 -0400

    MyMap passes

--- index.js
+++ index.js
@@ -1,43 +1,6 @@
 /*
-myEach(collection, callback)
 
-Parameter(s):
-  a collection (either an object or an array)
-  a callback function
-
-Return value:
-  The original collection
-
-Behavior:
-  Iterates over the collection of elements, passing each element in turn to
-  the callback function. Returns the original, unmodified, collection.
-
-Example function calls:
-  myEach([1, 2, 3], alert);
-  => alerts each number in turn and returns the original collection
-
-  myEach({one: 1, two: 2, three: 3}, alert);
-  => alerts each number value in turn and returns the original collection
 */
-
-
-// function myEach (collection, callback) {
-
-//   if (typeof collection === 'object') {
-//     for (let key in collection) {
-//       if(collection.hasOwnProperty(key)) {
-//         value = collection[key]
-//         callback(value)
-//       }
-//     }
-//   } else if (Array.isArray(collection)) {
-//     for (let i = 0; i < collection.length; i++) {
-//       callback(collection[i])
-//     }
-//   }
-// }
-
-
 
 function myEach (collection, callback) {
 
@@ -54,6 +17,20 @@ function myEach (collection, callback) {
   return collection
 }
 
-const unmodifiedTestObj = {one: 1, two: 2, three: 3, four: 4}
-const unmodifiedTestArr = [1, 2, 3, 4]
-myEach(unmodifiedTestObj, callback)
+
+function myMap (collection, callback) {
+  if (Array.isArray(collection)) {
+    let modArray = []
+    for (let i = 0; i < collection.length; i++) {
+      modArray.push(callback(collection[i]))
+    }
+    return modArray
+  } else if (!Array.isArray(collection)) {
+    let newArr = Object.values(collection)
+    let modArray = []
+    for (let i = 0; i < newArr.length; i++) {
+      modArray.push(callback(newArr[i]))
+    }
+    return modArray
+  }
+}
