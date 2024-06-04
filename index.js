bfa77537fade1e1c22c5ef0334bbf48f9079a35a
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-04 14:14:22 -0400

    Need to fix terminal errors before conitnuing

--- index.js
+++ index.js
@@ -2,26 +2,29 @@
 myReduce(collection, callback, acc)
 
 Parameter(s):
-
-    a collection (either an object or an array)
-    a callback function
-    a starting value for the accumulator (optional)
+  a collection (either an object or an array)
+  a callback function
+  a starting value for the accumulator (optional)
 
 Return value:
-
-    A single value
+  A single value
 
 Behavior:
+  Reduce iterates through a collection of values and boils it down into a single value.
+  acc (short for accumulator) starts at the value that's passed in as an argument, and
+  with each successive step is updated to the return value of callback. If a start
+  value is not passed to myReduce, the first value in the collection should be used as
+  the start value.
 
-Reduce iterates through a collection of values and boils it down into a single value. acc (short for accumulator) starts at the value that's passed in as an argument, and with each successive step is updated to the return value of callback. If a start value is not passed to myReduce, the first value in the collection should be used as the start value.
+  The callback is passed three arguments: the current value of acc, the current
+  element/value in our iteration, and a reference to the entire collection.
 
-The callback is passed three arguments: the current value of acc, the current element/value in our iteration, and a reference to the entire collection.
-
-Hint: For the case when a start value for the accumulator is not passed in as an argument, think about how you'll need to adjust your function to account for the fact that the first element of the collection has already been accounted for.
+  Hint: For the case when a start value for the accumulator is not passed in as an
+  argument, think about how you'll need to adjust your function to account for the
+  fact that the first element of the collection has already been accounted for.
 */
 
 function myEach (collection, callback) {
-
   if (Array.isArray(collection)) {
     for (let i = 0; i < collection.length; i++) {
       callback(collection[i])
@@ -34,7 +37,6 @@ function myEach (collection, callback) {
   }
   return collection
 }
-
 
 function myMap (collection, callback) {
   if (Array.isArray(collection)) {
@@ -52,3 +54,21 @@ function myMap (collection, callback) {
     return modArray
   }
 }
+
+function myReduce (collection, callback, acc) {
+  if (Array.isArray(collection)) {
+    for (let i = 0; i < collection.length; i++) {
+      acc = callback(acc, collection[i], collection)
+    }
+    return acc
+  } else {
+    let newArr = Object.values(collection)
+    for (let i = 0; i < newArr.length; i++) {
+      newArr = callback(newArr, collection[i], collection)
+    }
+    return newArr
+  }
+}
+
+myReduce([1, 2, 3], function(acc, val, collection) { return acc + val;}, 0);
+myReduce({one: 1, two: 2, three: 3}, function(acc, val, collection) { return acc + val; });
